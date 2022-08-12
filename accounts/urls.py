269c870f5788b5583f3245a184ca21e993fa50a0
flanne36 <flanne36@students.rowan.edu>
2022-08-12 10:23:16 -0400

    test

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.loginPage, name='login'),
     path('register/', views.registerPage, name='register'),
     path('logout/', views.logoutUser, name='logout'),
-    path('scoreboard/', views.scoreboard, name='scoreboard'),
-    # path('', views.test, name='test'),
+    # path('scoreboard/', views.scoreboard, name='scoreboard'),
+    path('scoreboard', views.test, name='test'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -7,21 +7,7 @@ from game.models import ScoreBoard, Wordsdata
 
 
 def test(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        form = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for ' + user)
-
-                return redirect('accounts:login')
-
-        context = {'form': form}
-        return render(request, 'accounts/register.html', context)
+    return render(request, 'test.html')
 
 # Create your views here.
 def loginPage(request):
