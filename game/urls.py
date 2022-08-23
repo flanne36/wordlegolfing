51851a3117159a226a0ec967740a789bcba732a7
flanne36 <flanne36@students.rowan.edu>
2022-08-23 12:29:19 -0400

    remove temp

--- game/urls.py
+++ game/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('stats/', views.stats, name='stats'),
     path('saveguess/', views.saveguess, name='saveguess'),
-    path('temp/', views.temp, name='temp')
 ]
